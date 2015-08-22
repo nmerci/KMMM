e6a4ab6ad36cbe95051d66f101e35400490b4ab4
nmerci <nmerci@mail.com>
2015-08-22 15:40:01 +0300

    Test data for an article is added.

--- R/main.R
+++ R/main.R
@@ -3,7 +3,6 @@ source("R/test.R")
 
 library(matrixStats)
 
-
 result <- numeric()
 for(j in 0:9)
 {
@@ -11,13 +10,12 @@ for(j in 0:9)
   
   n <- 100 + 100*j
   m <- 2
-  clusters <- replicate_numbers(2 + j*2, 50)
   
   sup_km <- numeric()
   sup_r <- numeric()
   for(i in 1:1000)
   {
-    res <- sapply(compare_estimators(n, m, clusters), abs, simplify=F)
+    res <- sapply(compare_estimators_grouped(n, m, 5), abs, simplify=F)
     #res <- sapply(compare_estimators(n, m, clusters), "^", 2, simplify = F)
     
     sup_km <- rbind(sup_km, apply(res$km, 2, max, na.rm=T))
@@ -26,9 +24,13 @@ for(j in 0:9)
   
   sup_km[sup_km > max(sup_r)] <- NA
   
-  result <- rbind(result, cbind(t(colMeans(sup_km, na.rm=T)), t(colMeans(sup_r, na.rm=T))))
+  result <- rbind(result, cbind(t(colMedians(sup_km, na.rm=T)), t(colMedians(sup_r, na.rm=T))))
 }
 
 result
 
 
+#temp plot
+plot(x=n, y=result[, 2], type="o", col="blue", main="ChiSq(2)", xlab="n", ylab="E[sup|F_est-F|]", ylim=c(0.06, 0.2))
+lines(x=n, y=result[, 4], type="o", pch=22, lty=2, col="red")
+legend(x=750, y=0.18, c("P-L", "R"), col=c("blue", "red"), pch=c(21, 22), lty=c(1, 2))
